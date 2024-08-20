a58e1043a56525d30062472c77fbfe53bd8364e4
Alumno <Alumno@LAB1507-04>
2024-08-20 17:27:08 -0500

    Agregar archivos de proyecto.

---
+++ Demo01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona
+            {
+                Nombres = "Ethan",
+                Apellidos = "Arredondo"
+            };
+            persona.AsignarNombresCompletos();
+            Console.WriteLine(persona.NombresCompletos);
+            Console.Read();
+            Rectangulo();
+        }
+        static void Rectangulo()
+        {
+            Rectangulo rectangulo = new Rectangulo();
+            rectangulo.Altura = 10;
+            rectangulo.Base = 20;
+            double perimetro = 0;
+            double area = 0;
+
+            area = rectangulo.CalcularArea();
+            perimetro = rectangulo.CalcularPerimetro();
+
+            Console.WriteLine("El área es: ");
+            Console.WriteLine(area);
+            Console.WriteLine("El perímetro es: ");
+            Console.WriteLine(perimetro);
+        }
+
+    }
+}
---
+++ Demo01/Rectangulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01
+{
+    internal class Rectangulo
+    {
+        public double Altura { get; set; }
+        public double Base { get; set; }
+
+        public double CalcularArea()
+        {
+            return Base * Altura;
+        }
+        public double CalcularPerimetro()
+        {
+            return 2 * (Base + Altura);
+        }
+    }
+}
